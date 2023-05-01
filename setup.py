f17bfd1e7a513039c85ab2f497d85847b8b24c45
InfiniteSwerve <jackofthestrand@gmail.com>
2023-05-01 13:55:20 -0700

    fixed setup issues

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="frequency analysis",
+    name="frequency-analysis",
     version="0.1.0",
     packages=["freq_analysis"],
     license="LICENSE",
